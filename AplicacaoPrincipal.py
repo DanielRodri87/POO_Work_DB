1ff26523238072f635cc26d8ea2108535553fbca
Daniel Rodrigues <capau.2020120iipi0064@aluno.ifpi.edu.br>
2024-11-27 16:31:22 -0300

    Resolvendo erro no docker

--- AplicacaoPrincipal.py
+++ AplicacaoPrincipal.py
@@ -1,16 +1,65 @@
-import psycopg2 as pg
+import redis
+import psycopg2
+# import AplicacaoPrincipal as ap
+from time import sleep
 
-class Postgre:
+import os
+
+import redis.exceptions
+
+class Conections:
     def __init__(self):
-        self.dbPostgre = pg.connect(
-            dbname="mydatabase",
-            user="root",
-            password="root",
-            host="localhost",
-            port="5432"
-        )
+        redis_host = os.getenv('HOST_TO_REDIS', 'localhost')
+        self.dbRedis = redis.Redis(host=redis_host, port=6379, decode_responses=True)
+
+        while True:
+            try:
+                post_host = os.getenv('HOST_TO_POSTGRES', 'localhost')
+                self.dbPostgre = psycopg2.connect(
+                    dbname="mydatabase",
+                    user="root",
+                    password="root",
+                    host=post_host,
+                    port="5432"
+                )
+                print("Conexao estabelecida")
+                break
+            except psycopg2.OperationalError:
+                print('Nao foi possivel conectar')
+                sleep(2)
     
-    def CriarTabela(self):
+    def fecharConexoes(self):
+        self.dbRedis.close()
+        self.dbPostgre.close()
+    
+    def tranferirinfos(self):
+        cursor = self.dbPostgre.cursor()
+        try:
+            for i in range(1, 1000001):
+                pacote = self.dbRedis.hgetall(i)
+                cursor.execute("""
+                    INSERT INTO pacotes (Destino, Origem, Peso, Tamanho)
+                    VALUES (%s, %s, %s, %s)
+                """, (
+                    pacote['Destino'],
+                    pacote['Origem'],
+                    pacote['Peso'],
+                    pacote['Tamanho']
+                ))
+                
+                self.dbPostgre.commit()
+                print(f"{i} registros transferidos.")
+            
+            self.dbPostgre.commit() 
+            print("Transferência concluída com sucesso.")
+            
+        except Exception as e:
+            print(f"Erro durante a transferência de dados: {e}")
+            self.dbPostgre.rollback()
+        finally:
+            cursor.close()
+
+    def CriaTabela(self):
         cursor = self.dbPostgre.cursor()
         cursor.execute("""
             CREATE TABLE IF NOT EXISTS pacotes (
@@ -23,101 +72,8 @@ class Postgre:
         self.dbPostgre.commit()
         cursor.close()
 
-    def inserirPacote(self, destino, origem, peso, tamanho):
-        cursor = self.dbPostgre.cursor()
-        cursor.execute("""
-            INSERT INTO pacotes (Destino, Origem, Peso, Tamanho) 
-            VALUES (%s, %s, %s, %s)
-        """, (destino, origem, peso, tamanho))
-        self.dbPostgre.commit()
-        cursor.close()
-
-    def consultarPacotes(self):
-        cursor = self.dbPostgre.cursor()
-        cursor.execute("SELECT * FROM pacotes")
-        pacotes = cursor.fetchall()
-        cursor.close()
-        return pacotes
-    
-    def atualizarPacote(self, destino, peso_novo, tamanho_novo):
-        cursor = self.dbPostgre.cursor()
-        cursor.execute("""
-            UPDATE pacotes 
-            SET Peso = %s, Tamanho = %s
-            WHERE Destino = %s
-        """, (peso_novo, tamanho_novo, destino))
-        self.dbPostgre.commit()
-        cursor.close()
-
-    def deletarPacote(self, destino):
-        cursor = self.dbPostgre.cursor()
-        cursor.execute("""
-            DELETE FROM pacotes 
-            WHERE Destino = %s
-        """, (destino,))
-        self.dbPostgre.commit()
-        cursor.close()
-
-    def fecharConexao(self):
-        self.dbPostgre.close()
-
-def exibir_menu():
-    print("\n")
-    print("=====================================")
-    print("         Sistema de Pacotes")
-    print("=====================================")
-    print("1. Inserir Pacote")
-    print("2. Consultar Pacotes")
-    print("3. Atualizar Pacote")
-    print("4. Deletar Pacote")
-    print("5. Sair")
-    print("=====================================")
-
-def main():
-    db = Postgre()
-    db.CriarTabela()
-    
-    while True:
-        exibir_menu()
-        opcao = input("Escolha uma opção: ")
-        
-        if opcao == '1':
-            destino = input("Digite o destino do pacote: ")
-            origem = input("Digite a origem do pacote: ")
-            peso = input("Digite o peso do pacote: ")
-            tamanho = input("Digite o tamanho do pacote: ")
-            db.inserirPacote(destino, origem, peso, tamanho)
-            print("Pacote inserido com sucesso!")
-
-        elif opcao == '2':
-            pacotes = db.consultarPacotes()
-            if pacotes:
-                print("Pacotes cadastrados:")
-                for pacote in pacotes:
-                    if (int(pacote[0]) % 1000 == 0):
-                        print(f"Destino: {pacote[0]}, Origem: {pacote[1]}, Peso: {pacote[2]}, Tamanho: {pacote[3]}")
-            else:
-                print("Nenhum pacote encontrado.")
-
-        elif opcao == '3':
-            destino = input("Digite o destino do pacote a ser atualizado: ")
-            peso_novo = input("Digite o novo peso do pacote: ")
-            tamanho_novo = input("Digite o novo tamanho do pacote: ")
-            db.atualizarPacote(destino, peso_novo, tamanho_novo)
-            print("Pacote atualizado com sucesso!")
-
-        elif opcao == '4':
-            destino = input("Digite o destino do pacote a ser deletado: ")
-            db.deletarPacote(destino)
-            print("Pacote deletado com sucesso!")
-
-        elif opcao == '5':
-            db.fecharConexao()
-            print("Conexão encerrada. Saindo do sistema...")
-            break
-
-        else:
-            print("Opção inválida. Tente novamente.")
-
 if __name__ == "__main__":
-    main()
+    con = Conections()
+    con.CriaTabela()
+    con.tranferirinfos()
+    con.fecharConexoes()
